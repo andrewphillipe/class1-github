f990128af23a76cd119a2e02d512dceea21bd7aa
Andrew Phillipe <andrewphillipe@hotmail.com>
2021-01-09 21:44:57 -0300

    Created Project

---
+++ src/course/Vect.java
@@ -0,0 +1,43 @@
+package course;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vect {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um valor: "); //define tamanho vetor
+		int n = sc.nextInt();
+		
+		Double[] vect = new Double[n];
+		
+		for(int i=0; i < n; i++) {
+			System.out.println("Digite a altura: "); //adiciona os elementos ao vetor
+			vect[i] = sc.nextDouble();
+			
+		}
+		
+		double sum =0;
+		for(int i =0 ; i < n ; i++) {	//soma os elementos do vetor 
+			sum += vect[i];
+			
+		}
+		
+		sum = sum / n; //calcula a média dos elementos
+		System.out.printf("A média das alturas é: %.2f%n", sum); 
+		
+		
+		
+		
+		
+		sc.close();
+			
+		
+		
+
+	}
+
+}
